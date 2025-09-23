89247b9a4ac83e2b8b488de1ed8f647fc9e4d9aa
Seu Nome <julianamoreno.est@gmail.com>
2025-09-22 22:22:42 -0300

    Criação de funções e alterações de algumas linhas.

--- src/UserSystem.php
+++ src/UserSystem.php
@@ -7,28 +7,30 @@ Class UserSystem
     public function __construct()
     {
         $this->users = [
-            ['id' => 1, 
-            'name' => 'João Silva', 
-            'email' => 'Joao@email.com', 
-            'password' => 'SenhaForte1'],
-
-            ['id' => 2, 
-            'name' => 'Maria Oliveira', 
-            'email' => 'Maria@email.com', 
-            'password' => 'Senha123'],
-
-            ['id' => 3, 
-            'name' => 'Pedro Souza', 
-            'email' => 'Pedro@email.com', 
-            'password' => 'Senha456']
-
+            [
+                'id' => 1, 
+                'name' => 'João Silva', 
+                'email' => 'Joao@email.com', 
+                'password' => password_hash('SenhaForte1', PASSWORD_DEFAULT)
+            ],
+            [
+                'id' => 2, 
+                'name' => 'Maria Oliveira', 
+                'email' => 'Maria@email.com', 
+                'password' => password_hash('Senha123', PASSWORD_DEFAULT)
+            ],
+            [
+                'id' => 3, 
+                'name' => 'Pedro Souza', 
+                'email' => 'Pedro@email.com', 
+                'password' => password_hash('Senha456',PASSWORD_DEFAULT)
+            ]
         ];
     }
-
+    
      private function isEmailValid(string $email): bool
      {
         return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
-
      }
 
      private function isPasswordStrong(string $password): bool
@@ -45,9 +47,45 @@ Class UserSystem
                 return $user;
             }
         }
-
         return null;
-
      }
+
+
+    public function register(string $name, string $email, string $password): string
+    {
+        if (!$this->isEmailValid($email)) {
+            return 'E-mail inválido.';
+        }
+
+        if (!$this->isPasswordStrong($password)) {
+            return 'Senha fraca.<br>A senha deve conter no mínimo 8 caracteres, pelo menos 1 letra maiúscula e 1 número';
+        }
+
+        if ($this->findUserByEmail($email)) {
+            return 'E-mail já foi cadastrado.';
+        }
+
+        $id = count($this->users) + 1;
+        $this->users[] = [
+            'id' => $id,
+            'name' => $name,
+            'email' => $email,
+            'password' => password_hash($password, PASSWORD_DEFAULT),
+        ];
+
+        return 'Usuário foi cadastrado com sucesso.';
+    }
+
+
+    public function login(string $email, string $password): string
+    {
+        $user = $this->findUserByEmail($email);
+
+        if ($user && password_verify($password, $user['password'])) {
+            return "Login realizado com sucesso!<br>Olá, {$user['name']}, seja bem-vindo! ";
+        }
+
+        return 'Login inválido.';
+    }
 }
 ?>
